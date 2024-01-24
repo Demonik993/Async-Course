8612aa7b4a9059c0ffe622cfda21e37cc243cc73
Demonik993 <wiecek.dom@gmail.com>
2024-01-24 20:58:58 +0100

    using API of reverse geolocation to find position and render data as well as passed codding challange

--- script.js
+++ script.js
@@ -85,6 +85,24 @@ const getJSON = function (url, errMsg = 'Something went wrong') {
     return response.json();
   });
 };
+const whereAmI = function (lat, lng) {
+  return fetch(
+    `https://geocode.xyz/${lat},${lng}?geoit=json&auth=147265429663054124807x78512`
+  )
+    .then(response => {
+      if (!response.ok)
+        throw new Error(
+          `There is a problem to check geocoding ${response.status}`
+        );
+      return response.json();
+    })
+    .then(data => {
+      const country = data.country;
+      const city = data.city;
+      return data.country.toLowerCase();
+    })
+    .catch(err => alert(err));
+};
 // const getCountry = function (country) {
 //   fetch(`https://restcountries.com/v3.1/name/${country}`)
 //     .then(response => {
@@ -136,15 +154,78 @@ const getCountry = function (country) {
       console.error(`${err}`);
       renderError(`Something went wrong ${err}, try again.`);
     })
-    .finally(() => (countriesContainer.style.opacity = 1));
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+      btn.style.opacity = 0;
+    });
 };
 btn.addEventListener('click', function () {
-  const location = navigator.language;
-  getJSON(`https://restcountries.com/v3.1/alpha/${location.slice(-2)}`)
-    .then(data => {
-      const country = data[0].name.common.toLowerCase();
-      if (!country) throw new Error('Something went wrong');
-      return country;
-    })
-    .then(data => getCountry(data));
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      whereAmI(lat, lng).then(data => getCountry(data));
+    },
+    error => {
+      console.error('Error getting the current position:', error.message);
+    }
+  );
+
+  //   getJSON(`https://restcountries.com/v3.1/alpha/${location.slice(-2)}`)
+  //     .then(data => {
+  //       const country = data[0].name.common.toLowerCase();
+  //       if (!country) throw new Error('Something went wrong');
+  //       return country;
+  //     })
+  //     .then(data => getCountry(data));
 });
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. 
+For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS 
+    coordinates, examples are below).
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful 
+location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API 
+and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided
+location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+4. Chain a .catch method to the end of the promise chain and log errors to the console
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. 
+This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject 
+the promise yourself, with a meaningful error message.
+
+PART 2
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, a
+nd plug it into the countries API that we have been using.
+7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, 
+    no need to type the same code)
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+147265429663054124807x78512 
+GOOD LUCK 😀
+*/
+// const whereAmI = function (lat, lng) {
+//   fetch(
+//     `https://geocode.xyz/${lat},${lng}?geoit=json&auth=147265429663054124807x78512`
+//   )
+//     .then(response => response.json())
+//     .then(data => {
+//       const country = data.country;
+//       const city = data.city;
+//       console.log(`You are in ${city}, ${country}`);
+//       console.log(data.country);
+//       return data.country.toLowerCase();
+//     })
+//     .catch(err => console.error(err));
+// };
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
